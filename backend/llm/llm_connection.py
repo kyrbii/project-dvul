25f667b79495001eadefc07c2da64dfc38a05009
Korbinian Eller <149487139+kyrbii@users.noreply.github.com>
2026-04-22 08:52:37 +0200

    Removed unnecessary import
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- backend/llm/llm_connection.py
+++ backend/llm/llm_connection.py
@@ -1,5 +1,4 @@
 # Gemini
-from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.messages import trim_messages
 
