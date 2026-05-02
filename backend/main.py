b67a604e9b018da6f99866893189aedfb69210f2
Korbinian Eller <korbinian@eller-software.de>
2026-05-02 08:34:23 +0000

    Commented out the Exception throwing lines

--- backend/main.py
+++ backend/main.py
@@ -78,29 +78,31 @@ async def upload_csv(file: UploadFile = File(...)):
     
     try:
         # delimiter erkennen
-        delimiter = detect_delimiter(file.file)
+        #delimiter = detect_delimiter(file.file)
 
         # header erkennen 
-        header_exists = has_header(file.file)
+        #header_exists = has_header(file.file)
         
-        if header_exists:
-            df = pd.read_csv(file.file, delimiter=delimiter)
-        else:
-            df = pd.read_csv(file.file, delimiter=delimiter, header=None)
+        #if header_exists:
+        #    df = pd.read_csv(file.file, delimiter=delimiter)
+        #else:
+        #    df = pd.read_csv(file.file, delimiter=delimiter, header=None)
+
+        df = pd.read_csv(file.file)
 
     except Exception:
         raise HTTPException(status_code=400, detail="CSV-Datei konnte nicht gelesen werden.")
     
-    chat_id = f"chat_{next(chat_counter)}"
+    chat_id = f"chat_{next(chat_counter)}" 
 
     summary = create_dataset_summary(df)
 
     chat_store[chat_id] = {             # @Korbi du musst dann in der get_llm_repsonse auch die summary miteinbeziehen fürs LLM
         "filename": file.filename,
-        "dataframe": df,
-        "summary": summary,
-        "messages": [],
-        "plots": []
+        "dataframe": df# ,
+        # "summary": summary,
+        # "messages": [],
+        # "plots": []
 }
     
     preview_df = df.head(5).replace([np.nan, np.inf, -np.inf], None)
@@ -112,6 +114,15 @@ async def upload_csv(file: UploadFile = File(...)):
         "summary": summary
         }
 
+@app.get("/description/{chat_id}")
+def get_description(chat_id: str):
+    if chat_id not in chat_store:
+        raise HTTPException(status_code=404, detail="Chat nicht gefunden.")
+    
+    return {
+        "chat_id": chat_id,
+        "summary": chat_store[chat_id]["description"]
+    }
 
 @app.get("/plots/{chat_id}/{plot_index}")               # für einen einzelnen Plot
 def get_plots(chat_id: str, plot_index: int):
